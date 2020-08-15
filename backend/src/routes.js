2321a80dca7862cea48c8ef8659aabd9efb675a7
Douglas Biason <douglas.biason@rbmweb.com.br>
2020-08-15 14:41:18 -0300

    right characteristics

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {celebrate, Segments, Joi} = require('celebrate');
+
+const OngController = require('./controller/OngController');
+const IncidentController = require('./controller/IncidentController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/session', SessionController.create);
+
+module.exports = routes;
